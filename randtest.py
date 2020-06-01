387f08caa1f47ade933995cf4683e9337326ae6d
Lucimore <55692210+Lucimore@users.noreply.github.com>
2020-06-01 09:12:14 +0300

    Update randtest.py

--- randtest.py
+++ randtest.py
@@ -3,8 +3,8 @@ import random
 #begin = float(input("Введите первое число\n"))
 #end = float(input("Введите второе число\n"))
 
-begin = 7.0  #Начало. Можно изменить
-end = 8.0  #Конец. Можно изменить
+begin = -10000.0  #Начало. Можно изменить
+end = 10000.0  #Конец. Можно изменить
 
 print("""#include <stdio.h>
 #include <limits.h>
@@ -16,10 +16,12 @@ int main(void)
 
 
 while begin < end:
-    begin += 0.01  # Здесь можно изменять шаг
-    p = random.randint(0, 1000)  # Точность - рандомное число от 0 до 1000, можно менять
-    w = random.randint(0, 1000)  # Ширина - рандомное число от 0 до 1000, можно менять
-    begin = float("{:.2f}".format(begin))  # Здесь можно указать точность, если закомментировать эту строку, будет 16
-    print(f'ft_printf("%{p}.{w}f\\n", {begin});')
+#    begin += 0.001
+    begin += random.uniform(0.0, 100.0)  # Здесь можно изменять шаг
+    p = random.randint(0, 15)  # Точность - рандомное число от 0 до 1000, можно>
+    w = random.randint(0, 100)  # Ширина - рандомное число от 0 до 1000, можно >
+    begin = float("{:.1f}".format(begin))  # Здесь можно указать точность, если>
+#    print(f'ft_printf("%{p}.{w}f\\n", {begin});')
+    print(f'ft_printf("%{p}.{w}Lf\\n", (long double){begin});')  # Тест для Lf
 
 print('}')
